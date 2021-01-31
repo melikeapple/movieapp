dfacbf6daf557965dbea6080de0801969006170a
melike elma <melikapple.80@gmail.com>
2021-01-31 22:06:30 +0300

    first commit

---
+++ src/components/partials/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Row,Col,Container} from "react-bootstrap";
+
+const SearchBar  = () => (
+    <>
+    <Container>
+        <Row>
+            <Col>
+                deneme
+            </Col>
+        </Row>
+    </Container>
+    </>
+)
+export default SearchBar
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: ["react-hot-loader/patch", path.join(__dirname, "src", "index.js")],
+        style: path.join(__dirname, "src", "assets", "sass", "style.scss"),
+    },
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /.(css|scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false,
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|ico)$/,
+                loader: "file-loader",
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            "react-dom": "@hot-loader/react-dom",
+        },
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "build"),
+        hot: true,
+        port: 3000,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.join(__dirname, "public", "index.html"),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+        }),
+    ],
+};
